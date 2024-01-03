2830ec740c42c7ce273444b3e5d70e7103c384c7
Nikita <nikisharma105@gmail.com>
2024-01-03 11:11:09 -0800

    Trying to add

---
+++ src/PrintHello.java
@@ -0,0 +1,11 @@
+//package com.practice;
+
+import java.sql.SQLOutput;
+
+public class PrintHello {
+    public static void main(String[] args) {
+        String name;
+        System.out.print("Hello World");
+    }
+
+}
